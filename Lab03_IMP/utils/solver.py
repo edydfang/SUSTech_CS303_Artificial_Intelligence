07e76f47ac58a3ed0c4bb73a9008483329d53b1f
Edward <fangyd1997@outlook.com>
2017-12-09 18:52:52 +0800

    add IC_CELF dev

--- Lab03_IMP/utils/solver.py
+++ Lab03_IMP/utils/solver.py
@@ -4,13 +4,13 @@ used to solve the influence maximization problem
 
 import random
 import logging
+import Queue as Q
 
 class Solver(object):
     '''
     Just a solver
     '''
     def __init__(self, graph, time_limit, random_seed, num_k):
-        
         if random_seed is not None:
             random.seed(random_seed)
         self.num_k = num_k
@@ -22,11 +22,32 @@ class Solver(object):
         '''
         control for the ic model
         '''
+        if self.heuristic:
+            # use degree discount heuristics
+            pass
+        else:
+            # use CELF
+            pass
         logging.debug("ic")
 
     def solve_lt(self):
         '''
         control for the LT model
         '''
+        if self.heuristic:
+            # use degree discount heuristics
+            pass
+        else:
+            # use CELF
+            pass
         logging.debug("lt")
+
+    def ic_celf(self):
+        '''
+        implementation of CELF
+        '''
+        pq = Q.PriorityQueue()
+        nodes = set(self.graph.vertices())
+        for idx in range(self.num_k):
+            pass
 
